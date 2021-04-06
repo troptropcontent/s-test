60746a059e7ddbd26a8fa344a00081ccf579d523
Tom <tomecrepont@gmail.com>
2021-04-06 09:14:00 +0200

    Quizz Show created

--- test-technique-study-advisor/app/controllers/quizzs_controller.rb
+++ test-technique-study-advisor/app/controllers/quizzs_controller.rb
@@ -6,11 +6,15 @@ class QuizzsController < ApplicationController
   end
 
   def show
-    raise
     @quizz = Quizz.find(
     params[:id]
     )
+    @user = current_user
+    @number_of_questions = @quizz.questions.count
     @quiz_taken_by_current_user = @quizz.attended_by_user(current_user)
+    @propositions_test = ["Proposition 1","Proposition 2","Proposition 3"]
+    @quizz_questions = @quizz.questions
+    @next_question_url = "/quizzs/#{@quizz.id}/questions/#{@quizz_questions.first.id}"
   end
   
   
--- test-technique-study-advisor/config/routes.rb
+++ test-technique-study-advisor/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'quizzs#index'
-  resources :quizzs, only: [:index, :show]
+  resources :quizzs, only: [:index, :show] do
+    resources :questions, only: [:show]
+  end
   resources :atempts, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
