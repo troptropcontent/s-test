23272b5c10ac0be1c16c1ee6bd292021087d9887
Tom <tomecrepont@gmail.com>
2021-04-07 08:52:52 +0200

    views upgrade

--- test-technique-study-advisor/app/controllers/quizzs_controller.rb
+++ test-technique-study-advisor/app/controllers/quizzs_controller.rb
@@ -11,18 +11,20 @@ class QuizzsController < ApplicationController
     )
     @user = current_user
     @status = @quizz.attempted_by_user(@user)
-    @score = @quizz.attempt_score(@user)
-    @number_of_questions = @quizz.questions.count
-    @propositions_test = ["Proposition 1","Proposition 2","Proposition 3"]
-    @quizz_questions = @quizz.questions
 
-    @next_question_url = if @status == "attempted"
-                            "/"
-                          elsif @status == "started"
-                            "/quizzs/#{@quizz.id}/questions/#{@quizz.last_question_answered_by_user(@user).next_question?.id}"
-                          else 
-                            "/quizzs/#{@quizz.id}/questions/#{@quizz.questions.first.id}"
-                         end
+    if @status == "attempted"
+      @next_question_url = "/"
+      @submission_date = @quizz.attempt_date(@user).strftime("%m/%d/%Y")
+      @score = @quizz.attempt_score(@user)
+    elsif @status == "started"
+      @next_question_url = "/quizzs/#{@quizz.id}/questions/#{@quizz.last_question_answered_by_user(@user).next_question?.id}"
+    else 
+      @propositions_test = ["Proposition 1","Proposition 2","Proposition 3"]
+      @number_of_questions = @quizz.questions.count
+      @next_question_url = "/quizzs/#{@quizz.id}/questions/#{@quizz.questions.first.id}"
+    end
+
+
   end
   
   
--- test-technique-study-advisor/app/models/quizz.rb
+++ test-technique-study-advisor/app/models/quizz.rb
@@ -44,14 +44,27 @@ class Quizz < ApplicationRecord
     if attempted_by_user(user) == "attempted"
       querry = AttemptAnswer.joins("INNER JOIN answers ON answers.id = attempt_answers.answer_id 
                                     INNER JOIN questions ON questions.id = answers.question_id
-                                    INNER JOIN quizzs ON quizzs.id = questions.quizz_id")
-                            .where("quizz_id = ? and attempt_answers.user_id = ?", self.id, user.id) 
-      score = querry.map{|answer| answer.correct? ? 1 : 0 }.sum/querry.count
+                                    INNER JOIN quizzs ON quizzs.id = questions.quizz_id").where("quizz_id = ? and attempt_answers.user_id = ?", quizz.id, user.id) 
+      score = (querry.map{|answer| answer.correct? ? 1 : 0 }.sum)/(querry.count)
       return score
     else
       attempted_by_user(user) == "not-started" ? "not-started" : "started"
     end
   end
+
+  def attempt_date(user)
+    if attempted_by_user(user) == "attempted"
+      querry = AttemptAnswer.joins("INNER JOIN answers ON answers.id = attempt_answers.answer_id 
+                                    INNER JOIN questions ON questions.id = answers.question_id
+                                    INNER JOIN quizzs ON quizzs.id = questions.quizz_id")
+                            .where("quizz_id = ? and attempt_answers.user_id = ?", self.id, user.id) 
+      date = querry.last.updated_at
+      return date
+    else
+      attempted_by_user(user) == "not-started" ? "not-started" : "started"
+    end
+  end
+
 
   def last_question_answered_by_user(user)
     querry = AttemptAnswer.joins("INNER JOIN answers ON answers.id = attempt_answers.answer_id 
